25c2aff3c81df4364e08f8e66f7366b5ee0ad1b9
Gupta, Kapil <Kapil.Gupta@cdk.com>
2018-06-26 17:39:24 +0530

    ensure Capacity and trim to Size method in ArrayList

---
+++ src/main/java/com/cdk/gist/collection/ArrayListTesting.java
@@ -0,0 +1,14 @@
+package com.cdk.gist.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTesting {
+
+	public static void main(String[] args) {
+		ArrayList list=new ArrayList<>();
+		list.ensureCapacity(12);
+		list.trimToSize();
+		//System.out.println(list.trimToSize(););
+	}
+}
